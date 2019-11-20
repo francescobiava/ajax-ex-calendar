20043b8098429b1c5a53f1acd79476050c7a8ad5
Francesco <franc.ns@gmail.com>
2019-11-20 17:27:54 +0100

    added function to get january 2018 days with holidays from ajax call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
 $(document).ready(function () {
   
-
+  var month = 1; //setto per adesso fisso a gennaio
+  getDays(month);
   
 });
+
+// FUNCTIONS
+// funzione per stampare i giorni del mese che gli passo
+function getDays(selectedMonth) {
+  // estraggo il numero di giorni del mese
+  var days = moment("2018-" + selectedMonth, "YYYY-M").daysInMonth();
+  // ciclo per tutti i giorni, appendo in pagina ogni giorno e aggiungo l'attributo per identificarlo
+  for (var i = 1; i <= days; i++) {
+    var date = moment('2018-' + selectedMonth + '-' + i, 'YYYY-M-D').format('YYYY-MM-DD');
+    var day = moment(date).format('DD dddd');
+    $('#calendar .jan').append('<li data-day=' + date + '>' + day + '</li>');
+  }
+  // richiamo la funzione della chiamata ajax
+  getHolidays();
+}
+
+// funzione chiamata Ajax
+function getHolidays() {
+  $.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+    method: 'GET',
+    success: function (data) {
+      // recupero dalla chiamata le festività
+      var holidays = data.response;
+      // ciclo per ogni festività
+      holidays.forEach(function(item) {
+        // recupero la data e il nome
+        var day = item.date;
+        var holiday = item.name;
+        // recupero il li con lo stesso attributo, gli assegno la classe per la festività e aggiungo il nome
+        $('li[data-day=' + day + ']').addClass('holiday').append('<span> ' + holiday + '</span>');
+      });
+    }
+  });
+}
